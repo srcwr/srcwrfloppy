b9a08f7bc9b10841e7f884933651bba6ec166b57
rtldg <rtldg@protonmail.com>
2025-07-10 04:05:24 +0000

    wip2

--- srcwrdelta/src/natives_delta.cpp
+++ srcwrdelta/src/natives_delta.cpp
@@ -23,10 +23,35 @@ void Extension_OnAllLoaded() {}
 
 static cell_t N_SRCWRDELTA_AsyncSaveReplay(IPluginContext* ctx, const cell_t* params)
 {
-	char *outbuf;
-	(void)ctx->LocalToString(params[1], &outbuf);
-	cell_t outbuflen = params[2];
-	return 1;
+	char *replayfmt;
+	(void)ctx->LocalToString(params[1], &replayfmt);
+	int replaysubversion = params[2];
+
+	if (0 != strcmp(replayfmt, "{SHAVITREPLAYFORMAT}{FINAL}") || replaysubversion != 0x09)
+	{
+		// TODO: Throw error
+	}
+
+	//int callback = params[3];
+	int value = params[4];
+
+	char *replayFolder;
+	(void)ctx->LocalToString(params[5], &replayFolder);
+
+	int style = params[6];
+	int track = params[7];
+	float time = params[8];
+	int steamid = params[9];
+	int preframes = params[10];
+	//int playerrecording = params[11];
+	int iSize = params[12];
+	int postframes = params[13];
+	int timestamp = params[14];
+	//int fZoneOffset = params[15];
+	bool saveCopy = params[16];
+	bool saveWR = params[17];
+
+	return 0; // native marked as void so return value doesn't matter...
 }
 
 extern const sp_nativeinfo_t DeltaNatives[] = {
