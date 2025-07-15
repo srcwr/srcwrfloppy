03f06408bcd5be2e9d86b7514497531e94ac4f74
rtldg <rtldg@protonmail.com>
2025-07-15 20:30:15 +0000

    v2.0.0: simplify header writing logic (by making sourcepawn do it instead!)

--- srcwr💾/src/lib.rs
+++ srcwr💾/src/lib.rs
@@ -7,21 +7,16 @@
 
 use std::ffi::c_char;
 use std::ffi::c_void;
-use std::io::BufWriter;
 use std::io::Write;
-use std::path::PathBuf;
 use std::ptr::NonNull;
-use std::str::FromStr;
 use std::sync::mpsc::Receiver;
 use std::sync::mpsc::Sender;
 use std::sync::mpsc::channel;
 use std::thread::JoinHandle;
 
-use byteorder::LittleEndian;
-use byteorder::WriteBytesExt;
-use extshared::cpp_extension_log_error;
 use extshared::ICellArray::ICellArray;
 use extshared::cpp_add_frame_action;
+use extshared::cpp_extension_log_error;
 use extshared::cpp_forward_execute;
 use extshared::cpp_forward_push_cell;
 use extshared::cpp_forward_push_string;
@@ -37,29 +32,16 @@ extshared::smext_conf_boilerplate_load_funcs!();
 static mut SENDER: Option<Sender<Msg>> = None;
 static mut THREAD: Option<JoinHandle<()>> = None;
 
-const REPLAY_VERSION: &[u8] = b"9:{SHAVITREPLAYFORMAT}{FINAL}\n";
-const FRAME_T_SIZE: usize = 10 * 4;
-
 #[derive(Debug)]
 struct Msg {
 	forward:          NonNull<c_void>,
 	value:            i32,
-	replayFolderOrig: String,
-	replayFolder:     PathBuf,
-	map:              String,
-	style:            i32,
-	track:            i32,
-	time:             f32,
-	steamid:          i32,
-	preframes:        i32,
+	wrpath:           String,
+	copypath:         String,
+	header:           Vec<u8>,
 	playerrecording:  *mut ICellArray,
-	iSize:            i32,
-	postframes:       i32,
-	timestamp:        i32,
-	fZoneOffset:      [f32; 2],
-	saveCopy:         bool,
-	saveWR:           bool,
-	tickrate:         f32,
+	totalframes:      usize,
+	sm_friendly_path: String,
 }
 unsafe impl Send for Msg {} // so we can store the pointers...
 
@@ -97,26 +79,20 @@ pub extern "C" fn rust_KILL_replay_thread() {
 pub extern "C" fn rust_post_to_replay_thread(
 	forward: NonNull<c_void>,
 	value: i32,
-	replayFolderOrig: *const c_char,
-	replayFolder: *const c_char,
-	map: *const c_char,
-	style: i32,
-	track: i32,
-	time: f32,
-	steamid: i32,
-	preframes: i32,
+	wrpath: *const c_char,
+	copypath: *const c_char,
+	header: *const u8,
+	headersize: usize,
 	playerrecording: *mut ICellArray,
-	iSize: i32,
-	postframes: i32,
-	timestamp: i32,
-	fZoneOffset: *const f32,
-	saveCopy: bool,
-	saveWR: bool,
-	tickrate: f32,
+	totalframes: usize,
+	sm_friendly_path: *const c_char,
 ) {
-	let replayFolderOrig = strxx(replayFolderOrig, false, 0).unwrap().to_string();
-	let replayFolder = PathBuf::from_str(strxx(replayFolder, false, 0).unwrap()).unwrap();
-	let map = strxx(map, false, 0).unwrap().to_string();
+	let wrpath = strxx(wrpath, false, 0).unwrap_or_default().to_string();
+	let copypath = strxx(copypath, false, 0).unwrap_or_default().to_string();
+	let sm_friendly_path = strxx(sm_friendly_path, false, 0).unwrap().to_string();
+
+	let header = unsafe { std::slice::from_raw_parts(header, headersize).to_vec() };
+
 	//println!("hello from poster!");
 	unsafe {
 		if let Some(sender) = &SENDER {
@@ -124,111 +100,63 @@ pub extern "C" fn rust_post_to_replay_thread(
 				.send(Msg {
 					forward,
 					value,
-					replayFolderOrig,
-					replayFolder,
-					map,
-					style,
-					track,
-					time,
-					steamid,
-					preframes,
+					wrpath,
+					copypath,
+					header,
 					playerrecording,
-					iSize,
-					postframes,
-					timestamp,
-					fZoneOffset: [fZoneOffset.read(), fZoneOffset.add(1).read()],
-					saveCopy,
-					saveWR,
-					tickrate,
+					totalframes,
+					sm_friendly_path,
 				})
 				.unwrap();
 			//println!("posted!");
 		}
 	}
-	//
-}
-
-fn write_replay_header(file: &mut BufWriter<std::fs::File>, msg: &Msg) {
-	let _ = file.write(REPLAY_VERSION);
-
-	let _ = write!(file, "{}\0", msg.map);
-	let _ = file.write_i8(msg.style as i8);
-	let _ = file.write_i8(msg.track as i8);
-	let _ = file.write_i32::<LittleEndian>(msg.preframes);
-
-	let _ = file.write_i32::<LittleEndian>(msg.iSize - msg.preframes - msg.postframes);
-	let _ = file.write_f32::<LittleEndian>(msg.time);
-	let _ = file.write_i32::<LittleEndian>(msg.steamid);
-
-	let _ = file.write_i32::<LittleEndian>(msg.postframes);
-	let _ = file.write_f32::<LittleEndian>(msg.tickrate);
-
-	let _ = file.write_f32::<LittleEndian>(msg.fZoneOffset[0]);
-	let _ = file.write_f32::<LittleEndian>(msg.fZoneOffset[1]);
 }
 
 fn replay_thread(recv: Receiver<Msg>) {
 	while let Ok(msg) = recv.recv() {
 		//println!("received {msg:?}");
-		let mut callbackPath = String::new();
 
-		let mut fCopy = if msg.saveCopy {
-			callbackPath = format!("copy/{}_{}_{}.replay", msg.timestamp, msg.steamid, msg.map);
-			if let Ok(f) = std::fs::File::create(msg.replayFolder.join(&callbackPath)).map(std::io::BufWriter::new) {
-				Some(f)
-			} else {
-				log_error(format!("Failed to open 'copy' replay file for writing. ('{callbackPath}')"));
-				None
-			}
-		} else {
-			None
-		};
-		let mut fWR = if msg.saveWR {
-			callbackPath = if msg.track > 0 {
-				format!("{}/{}{}.replay", msg.style, msg.map, msg.track)
-			} else {
-				format!("{}/{}.replay", msg.style, msg.map)
-			};
-			if let Ok(f) = std::fs::File::create(msg.replayFolder.join(&callbackPath)).map(std::io::BufWriter::new) {
-				Some(f)
-			} else {
-				log_error(format!("Failed to open WR replay file for writing. ('{callbackPath}')"));
-				None
-			}
-		} else {
-			None
-		};
+		let mut fcopy = None;
+		let mut fwr = None;
 
-		if fCopy.is_none() && fWR.is_none() {
-			log_error(format!(
-				"Failed to open WR and 'copy' replay files for writing [U:1:{}] style={} track={} map={}",
-				msg.steamid, msg.style, msg.track, msg.map
-			));
+		if !msg.copypath.is_empty() {
+			if let Ok(f) = std::fs::File::create(&msg.copypath).map(std::io::BufWriter::new) {
+				fcopy = Some(f);
+			} else {
+				log_error(format!("Failed to open 'copy' replay file for writing. ('{}')", msg.copypath));
+			}
+		}
+
+		if !msg.wrpath.is_empty() {
+			if let Ok(f) = std::fs::File::create(&msg.wrpath).map(std::io::BufWriter::new) {
+				fwr = Some(f);
+			} else {
+				log_error(format!("Failed to open WR replay file for writing. ('{}')", msg.wrpath));
+			}
+		}
+
+		if fcopy.is_none() && fwr.is_none() {
 			continue;
 		}
 
-		if let Some(f) = &mut fWR {
-			write_replay_header(f, &msg);
-		}
-
-		if let Some(f) = &mut fCopy {
-			write_replay_header(f, &msg);
-		}
-
 		let cellarray = unsafe { &mut *msg.playerrecording };
-		let frames = unsafe { std::slice::from_raw_parts(cellarray.data as *const u8, FRAME_T_SIZE * msg.iSize as usize) };
+		let frames =
+			unsafe { std::slice::from_raw_parts(cellarray.data as *const u8, cellarray.blocksize * 4 * msg.totalframes) };
 
-		if let Some(f) = &mut fWR {
-			let _ = f.write(frames);
+		if let Some(f) = &mut fwr {
+			let _ = f.write_all(&msg.header);
+			let _ = f.write_all(frames);
 		}
-		if let Some(f) = &mut fCopy {
-			let _ = f.write(frames);
+		if let Some(f) = &mut fcopy {
+			let _ = f.write_all(&msg.header);
+			let _ = f.write_all(frames);
 		}
 
-		if let Some(mut f) = fCopy {
+		if let Some(mut f) = fcopy {
 			let _ = f.flush();
 		}
-		if let Some(mut f) = fWR {
+		if let Some(mut f) = fwr {
 			let _ = f.flush();
 		}
 
@@ -239,8 +167,7 @@ fn replay_thread(recv: Receiver<Msg>) {
 					forward: msg.forward,
 					//saved: ?
 					value:   msg.value,
-					// note the \0
-					path:    format!("{}/{}\0", msg.replayFolderOrig, callbackPath),
+					path:    msg.sm_friendly_path,
 				})) as *mut _ as *mut c_void,
 			);
 		}
@@ -249,10 +176,11 @@ fn replay_thread(recv: Receiver<Msg>) {
 
 unsafe extern "C" fn do_callback(data: *mut c_void) {
 	unsafe {
-		let data = Box::from_raw(data as *mut Callbacker);
+		let mut data = Box::from_raw(data as *mut Callbacker);
 		cpp_forward_push_cell(data.forward, 1); // saved -- TODO
 		//println!("data.value: {:x}", data.value);
 		cpp_forward_push_cell(data.forward, data.value);
+		data.path.push('\0');
 		cpp_forward_push_string(data.forward, data.path.as_ptr());
 		cpp_forward_execute(data.forward, &mut 0);
 	}
--- srcwr💾/src/natives.cpp
+++ srcwr💾/src/natives.cpp
@@ -4,8 +4,6 @@
 #include "../../../srcwrtimer/extshared/src/extension.h"
 #include "../../../srcwrtimer/extshared/src/coreident.hpp"
 
-#include <ICellArray.h>
-
 
 extern "C" {
 
@@ -13,27 +11,15 @@ void rust_setup_replay_thread();
 void rust_KILL_replay_thread();
 
 void rust_post_to_replay_thread(
-	  IChangeableForward* forward
-	// what to pass along to the callback
-	, int value
-	// blahlahalkahlalkhlk
-	, const char* replayFolderOrig
-	, const char* replayFolder
-	, const char* map
-	// copy of SaveReplay() params
-	, int style
-	, int track
-	, int time
-	, int steamid
-	, int preframes
+	  IChangeableForward* forward // what to pass along to the callback
+	, int value // what to pass along to the callback
+	, const char* wrpath
+	, const char* copypath
+	, const char* header
+	, size_t headersize
 	, void* playerrecording
-	, int iSize
-	, int postframes
-	, int timestamp
-	, int* fZoneOffset
-	, bool saveCopy
-	, bool saveWR
-	, int tickrate
+	, size_t totalframes
+	, const char* sm_friendly_path
 );
 
 }
@@ -63,44 +49,25 @@ void Extension_OnAllLoaded() {}
 
 static cell_t N_SRCWRFloppy_AsyncSaveReplay(IPluginContext* ctx, const cell_t* params)
 {
-	char *replayfmt;
-	(void)ctx->LocalToString(params[1], &replayfmt);
-	int replaysubversion = params[2];
+	cell_t callback = params[1];
+	int value = params[2];
 
-	if (0 != strcmp(replayfmt, "{SHAVITREPLAYFORMAT}{FINAL}") || replaysubversion != 0x09)
-	{
-		// TODO: Throw error
-	}
+	char *wrpath_friendly, wrpath[PLATFORM_MAX_PATH], *copypath_friendly, copypath[PLATFORM_MAX_PATH];
+	(void)ctx->LocalToString(params[3], &wrpath_friendly);
+	(void)ctx->LocalToString(params[4], &copypath_friendly);
+	if (wrpath_friendly[0]) smutils->BuildPath(Path_Game, wrpath, sizeof(wrpath), "%s", wrpath_friendly);
+	if (copypath_friendly[0]) smutils->BuildPath(Path_Game, copypath, sizeof(copypath), "%s", copypath_friendly);
 
-	cell_t callback = params[3];
-	int value = params[4];
+	char* header;
+	(void)ctx->LocalToString(params[5], &header);
+	int headersize = params[6];
 
-	char *replayFolderOrig, replayFolder[PLATFORM_MAX_PATH];
-	(void)ctx->LocalToString(params[5], &replayFolderOrig);
-	smutils->BuildPath(Path_Game, replayFolder, sizeof(replayFolder), "%s", replayFolderOrig);
-	char *map;
-	(void)ctx->LocalToString(params[6], &map);
+	void* playerrecording;
+	Handle_t playerrecording_handle = params[7];
+	if (HandleError err = ReadHandleCoreIdent(playerrecording_handle, g_ArrayListType, &playerrecording); err != HandleError_None)
+		return ctx->ThrowNativeError("Invalid ArrayList Handle %x (error %d)", playerrecording_handle, err);
 
-	int style = params[7];
-	int track = params[8];
-	int time = params[9];
-	int steamid = params[10];
-	int preframes = params[11];
-	ICellArray *arraylist;
-	Handle_t playerrecording = params[12];
-	if (HandleError err = ReadHandleCoreIdent(playerrecording, g_ArrayListType, (void **)&arraylist); err != HandleError_None)
-		return ctx->ThrowNativeError("Invalid ArrayList Handle %x (error %d)", playerrecording, err);
-	//printf("playerrecording: %x\narraylist: %x\n", playerrecording, arraylist);
-	//printf("value: %x\n", value);
-	int iSize = params[13];
-	int postframes = params[14];
-	int timestamp = params[15];
-	int* fZoneOffset;
-	if (SP_ERROR_NONE != ctx->LocalToPhysAddr(params[16], (cell_t**)&fZoneOffset))
-		return ctx->ThrowNativeError("fuck");
-	bool saveCopy = params[17];
-	bool saveWR = params[18];
-	int tickrate = params[19];
+	int totalframes = params[8];
 
 	IChangeableForward* fw = forwards->CreateForwardEx(
 		  NULL
@@ -111,33 +78,22 @@ static cell_t N_SRCWRFloppy_AsyncSaveReplay(IPluginContext* ctx, const cell_t* p
 		, Param_Any // value
 		, Param_String // sPath
 	);
-	if (!fw)
-		return ctx->ThrowNativeError("failed to create a forward...");
-	if (!fw->AddFunction(ctx, callback))
+	if (!fw || !fw->AddFunction(ctx, callback))
 	{
-		forwards->ReleaseForward(fw);
-		return ctx->ThrowNativeError("Failed to add callback to forward thing...");
+		if (fw) forwards->ReleaseForward(fw);
+		return ctx->ThrowNativeError("Failed to create callback forward");
 	}
 
 	rust_post_to_replay_thread(
 		  fw
 		, value
-		, replayFolderOrig
-		, replayFolder
-		, map
-		, style
-		, track
-		, time
-		, steamid
-		, preframes
-		, arraylist
-		, iSize
-		, postframes
-		, timestamp
-		, fZoneOffset
-		, saveCopy
-		, saveWR
-		, tickrate
+		, wrpath
+		, copypath
+		, header
+		, headersize
+		, playerrecording
+		, totalframes
+		, wrpath_friendly[0] ? wrpath_friendly : copypath_friendly
 	);
 
 	return 0; // native marked as void so return value doesn't matter...
